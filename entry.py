3b826172f1376443fffb8d2d2146f166dd59d597
Joseph Eichenhofer <jeichenhofer@github.com>
2017-12-06 12:02:37 -0600

    implemented single tuple entry for user

--- entry.py
+++ entry.py
@@ -1,8 +1,65 @@
-from flask import Blueprint
+import sqlite3
+import os
+
+import re
+
+from validate_email import validate_email
+from flask import Blueprint, render_template, abort, request
+
 
 entry_api = Blueprint('entry_api', __name__)
 
 
+db_connection = sqlite3.connect(
+    os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        'ym.db'
+    )
+)
+
+
+def check_user():
+    return True
+
+
 @entry_api.route("/entry")
 def entry_index():
-    return "entry index"
+    if not check_user():
+        abort(403)
+    return render_template('entry/index.html')
+
+
+@entry_api.route("/entry/user", methods=['POST', 'GET'])
+def user_entry():
+    message = None
+    if request.method == 'GET':
+        return render_template('entry/user.html', message=message)
+    elif request.method == 'POST':
+        try:
+            u_name = request.form['u_name']
+            email = request.form['email']
+        except KeyError:
+            message = 'bad form data'
+            return render_template('entry/user.html', message=message)
+        if (not re.match(r'[a-z0-9]+', u_name)) or len(u_name) > 40:
+            message = "u_name must be alphanumeric and at most 40 characters"
+            return render_template('entry/user.html', message=message)
+        if not validate_email(email):
+            message = "invalid email format"
+            return render_template('entry/user.html', message=message)
+        try:
+            cur = db_connection.cursor()
+            cur.execute("INSERT INTO USER VALUES (NULL, ?, ?, strftime('%s', 'now'))", (u_name, email))
+            uid = cur.lastrowid
+            db_connection.commit()
+            inserted_row = cur.execute("SELECT * FROM USER WHERE UID=?", (uid,))
+            message = 'inserted new user successfully: ' + str(inserted_row.fetchone())
+            return render_template('entry/user.html', message=message)
+        except sqlite3.Error as err:
+            print(err)
+            db_connection.rollback()
+            message = 'error inserting tuple (' + str(err) + ')'
+            return render_template('entry/user.html', message=message)
+
+    else:
+        abort(405)
--- ymdb_server.py
+++ ymdb_server.py
@@ -1,13 +1,9 @@
-import sqlite3
-import os
-
 from flask import Flask, flash, redirect, render_template, request, session, abort
 
 from browse import browse_api
 from entry import entry_api
 from search import search_api
 
-global db_connection
 
 app = Flask(__name__)
 app.register_blueprint(browse_api)
@@ -17,17 +13,8 @@ app.register_blueprint(search_api)
 
 @app.route('/')
 def index():
-    curs = db_connection.cursor()
-    curs.execute('SELECT * FROM MOVIE')
-    rows = curs.fetchall()
-    return render_template('index.html', rows=rows)
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    db_connection = sqlite3.connect(
-        os.path.join(
-            os.path.dirname(os.path.realpath(__file__)),
-            'ym.db'
-        )
-    )
     app.run()
