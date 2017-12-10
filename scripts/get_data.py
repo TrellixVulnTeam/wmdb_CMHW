6864b0d1c9e5fc68a2e87d08737feeb04d9102ab
Joseph Eichenhofer <jeichenhofer@github.com>
2017-12-10 01:21:56 -0600

    emptied database to prep for data crawling

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -12,9 +12,10 @@ payload = {'i': 'tt0000001', 'type': 'movie', 'apikey': api_key}
 r = requests.get(movie_api, params=payload)
 
 
-mid_vals = random.sample(range(1, 3135393), 100000)
+mid_vals = random.sample(range(1, 3135393), 101010)
 
 for i in mid_vals:
     imdb_id = 'tt' + format(i, '07')
     payload = {'i': imdb_id, 'type': 'movie', 'apikey': api_key}
+    data = requests.get(movie_api, params=payload).json()
     print(payload)
