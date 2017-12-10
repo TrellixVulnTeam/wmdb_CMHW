c46e382fa076e15975e6af344a83acf733e154c6
Joseph Eichenhofer <jeichenhofer@github.com>
2017-12-10 01:07:26 -0600

    fixed typo method -> methods to fix runtime error

--- search.py
+++ search.py
@@ -9,33 +9,35 @@ db_path = os.path.join(
             'ym.db'
         )
 
+
 @search_api.route("/search")
 def search_index():
     return "search index"
 
-@search_api.route('/search/rating', method = 'POST')
+
+@search_api.route('/search/rating', methods=['POST'])
 def search_rating():
     movie = request.form['movie']
     with sql.connect(db_path) as con:
         cur = con.cursor()
-        query = "SELECT MOVIE.title, AVG(REVIEW.rating) as rating  FROM MOVIE, REVIEW WHERE MOVIE.MID == REVIEW.MID AND MOVIE.title LIKE ? GROUP BY MOVIE.title", (moive)
+        query = "SELECT MOVIE.title, AVG(REVIEW.rating) as rating  FROM MOVIE, REVIEW WHERE MOVIE.MID == REVIEW.MID AND MOVIE.title LIKE ? GROUP BY MOVIE.title", (movie,)
         cur.execute(query)
         result = cur.fetchall()
     return render_template('search/search_rating.html', result=result, moiveTile=movie)
 
 
-@search_api.route('/search/director', method = 'POST')
+@search_api.route('/search/director', methods=['POST'])
 def search_director_famours_movie():
     director = request.form['directorName']
     with sql.connect(db_path) as con:
         cur = con.cursor()
-        query = "SELECT MOVIE.title, DIRECTOR.given_name FROM MOVIE, DIRECTOR WHERE MID = famous_for_MID AND DIRECTOR.given_name LIKE ?", (director)
+        query = "SELECT MOVIE.title, DIRECTOR.given_name FROM MOVIE, DIRECTOR WHERE MID = famous_for_MID AND DIRECTOR.given_name LIKE ?", (director,)
         cur.execute(query)
         rows = cur.fetchall()
     return render_template('search/search_directorMoive.html', rows=rows, directorName=director)
 
 
-@search_api.route('/search/user', method  = 'POST')
+@search_api.route('/search/user', methods=['POST'])
 def search_user():
     user_email = request.form['email']
     with sql.connect(db_path) as con:
@@ -44,9 +46,3 @@ def search_user():
         cur.execute(query)
         result = cur.fetchall()
     return render_template('search/search_userName.html', userName=result, userEmail=user_email)
-
-
-
-    
-
-                                
